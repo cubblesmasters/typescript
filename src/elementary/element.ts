7f00be292da2fe7ad8360d6945ceecf40d2e8fe7
edwingamboa <edwingamboa355@gmail.com>
2019-05-08 11:06:55 -0500

    cubbles/cubbles#22 Add toUppperCase method for demo purposes

--- src/elementary/element.ts
+++ src/elementary/element.ts
@@ -1,5 +1,6 @@
 import "./element.sss";
 import { CubxComponentPrototype } from "./cubx-component-defs";
+import { toUpperCase } from "./toUpperCase";
 
 // Declare the CubxComponent to make it available
 declare function CubxComponent(prototype: CubxComponentPrototype): void;
@@ -46,7 +47,7 @@ CubxComponent({
     this.updateOutputText(inputText);
   },
   updateOutputText(inputText: string): void {
-    let upperCaseText = inputText.toUpperCase();
+    let upperCaseText = toUpperCase(inputText);
     this.outputText.innerText = upperCaseText;
     this.setOutputText(upperCaseText);
   }
---
+++ src/elementary/toUpperCase.ts
@@ -0,0 +1,3 @@
+export function toUpperCase (textToConvert:string) {
+    return textToConvert.toUpperCase();
+}
